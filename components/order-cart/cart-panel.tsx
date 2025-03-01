6226e72329155566fce3031118b857e5c322e3ee
Winzyl Olvido <prowinzil@gmail.com>
2025-03-01 22:01:00 +0800

    minor adjustment

--- components/order-cart/cart-panel.tsx
+++ components/order-cart/cart-panel.tsx
@@ -19,6 +19,7 @@ export function CartPanel({
     onToggleCollapse,
     onRemoveFromCart
 }: CartPanelProps) {
+    
     const renderCartItem = ({ item }: { item: CartItem }) => (
         <View style={[
             styles.cartItemWrapper,
