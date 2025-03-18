d4a51c74a5b58ef909942f0eed251b9db41a0e3a
Winzyl Olvido <prowinzil@gmail.com>
2025-03-18 18:21:28 +0800

    temporarily disable purchase orders

---
+++ app/(tabs)/(purchase-orders)/_layout.tsx
@@ -0,0 +1,21 @@
+// import { Stack } from 'expo-router';
+
+// export default function PurchaseOrdersLayout() {
+    
+//     return (
+//         <Stack
+//             screenOptions={{
+//                 headerStyle: {
+//                     // backgroundColor: 'blue',
+//                 },
+//                 headerShown: false,
+//                 headerTintColor: '#fff',
+//                 headerTitleStyle: {
+//                     fontWeight: 'bold',
+//                 },
+//             }}>
+
+//             <Stack.Screen name="purchase-orders"  options={{ title: "Purchase Orders" }}/>
+//         </Stack>
+//     );
+// }
