aa9252c023b4be3b0af82d3e5381c825cc6cfc3b
Winzyl Olvido <prowinzil@gmail.com>
2025-03-19 13:22:08 +0800

    purchase orders tab enabled

--- app/(tabs)/(purchase-orders)/_layout.tsx
+++ app/(tabs)/(purchase-orders)/_layout.tsx
@@ -1,21 +1,21 @@
-// import { Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 
-// export default function PurchaseOrdersLayout() {
+export default function PurchaseOrdersLayout() {
     
-//     return (
-//         <Stack
-//             screenOptions={{
-//                 headerStyle: {
-//                     // backgroundColor: 'blue',
-//                 },
-//                 headerShown: false,
-//                 headerTintColor: '#fff',
-//                 headerTitleStyle: {
-//                     fontWeight: 'bold',
-//                 },
-//             }}>
+    return (
+        <Stack
+            screenOptions={{
+                headerStyle: {
+                    // backgroundColor: 'blue',
+                },
+                headerShown: false,
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}>
 
-//             <Stack.Screen name="purchase-orders"  options={{ title: "Purchase Orders" }}/>
-//         </Stack>
-//     );
-// }
+            <Stack.Screen name="purchase-orders"  options={{ title: "Purchase Orders" }}/>
+        </Stack>
+    );
+}
