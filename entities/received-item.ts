e4e876e73e10f73e92ba07d8f763be3abdd92315
Winzyl Olvido <prowinzil@gmail.com>
2025-03-21 17:17:15 +0800

    added id on received item entity

--- entities/received-item.ts
+++ entities/received-item.ts
@@ -4,6 +4,7 @@ import { Product } from '@/entities/product';
  * Represents a received item (aka delivery line) in the system.
  */
 export interface ReceivedItem {
+    id: number;
     product: Product;
     quantity: number;
     total: number;
