fe654a5bef1f26492fb4bdd58cb082ba1a50fd35
Winzyl Olvido <prowinzil@gmail.com>
2025-03-28 16:28:18 +0800

    initial purchase order context implemented

---
+++ features/purchase-order-feature/context/purchase-order-context.tsx
@@ -0,0 +1,66 @@
+import { PurchaseOrder } from "@/entities/purchase-order";
+import { PurchaseOrderRepository } from "@/repositories/purchase-order-repository";
+import { createContext, ReactNode, useState } from "react";
+
+interface PurchaseOrderContextType {
+    selectedPurchaseOrder: PurchaseOrder | null;
+    purchaseOrders: PurchaseOrder[];
+    reLoadPurchaseOrders: () => Promise<void>;
+    getPurchaseOrderByid: (id: number) => Promise<PurchaseOrder | undefined>;
+    isLoading: boolean;
+}
+
+const PurchaseOrderContext = createContext<PurchaseOrderContextType | undefined>(undefined);
+
+export const PurchaseOrderProvider = ({ children }: { children: ReactNode }) => {
+    const [selectedPurchaseOrder, setSelectedPurchaseOrder] = useState<PurchaseOrder | null>(null);
+    const [purchaseOrders, setPurchaseOrders] = useState<PurchaseOrder[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const purchaseOrderRepository = new PurchaseOrderRepository(); // Assuming you have a PurchaseOrderRepository
+
+    const loadPurchaseOrders = async () => {
+        setIsLoading(true);
+        try {
+            const orders = await purchaseOrderRepository.getAll();
+            setPurchaseOrders(orders);
+        } catch (error) {
+            console.error("Error loading purchase orders:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const reLoadPurchaseOrders = async () => {
+        await loadPurchaseOrders();
+    };
+
+    const getPurchaseOrderByid = async (id: number): Promise<PurchaseOrder | undefined> => {
+        try{
+            const purchaseOrder = await purchaseOrderRepository.getById(id);
+            if (purchaseOrder) {
+                return purchaseOrder;
+            } else {
+                console.warn(`Purchase order with id ${id} not found`);
+                return undefined;
+            }
+        } catch (error) {
+            console.error(`Error fetching purchase order with id ${id}:`, error);
+            throw error;
+        }
+    };
+
+    return (
+        <PurchaseOrderContext.Provider
+            value={{
+                selectedPurchaseOrder,
+                purchaseOrders,
+                reLoadPurchaseOrders,
+                getPurchaseOrderByid,
+                isLoading,
+            }}
+        >
+            {children}
+        </PurchaseOrderContext.Provider>
+    );
+};
