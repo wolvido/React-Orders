681308ca9923a5f079099b7d6644391654b1f9be
Winzyl Olvido <prowinzil@gmail.com>
2025-04-02 11:03:18 +0800

    added logic for purchase orders app layer

--- src/app/(tabs)/(purchase-orders)/purchase-orders.tsx
+++ src/app/(tabs)/(purchase-orders)/purchase-orders.tsx
@@ -1,23 +1,53 @@
 import { View } from "react-native";
 import PurchaseOrderList from "@/src/features/purchase-order-feature/components/purchase-order-list";
 import { usePurchaseOrder } from "@/src/features/purchase-order-feature/context/purchase-order-context";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { PurchaseOrder } from "@/src/entities/purchase-order/type/purchase-order";
+import { router } from "expo-router";
 
 export default function PurchaseOrdersScreen() {
 
-    const { purchaseOrders, reloadPurchaseOrders } = usePurchaseOrder();
+    const [ isLoading, setIsLoading ] = useState(false);
 
+    const { purchaseOrders, reloadPurchaseOrders, getPurchaseOrderById, setSelectedPurchaseOrder } = usePurchaseOrder();
     useEffect(() => {
         if (purchaseOrders.length === 0) {
             reloadPurchaseOrders();
         }
     }, []);
 
+    // receive PO
+    const [purchaseOrderToReceive, setPurchaseOrderToReceive] = useState<PurchaseOrder | null>(null);
+    const handleReceivePo = async (id: number) => {
+        setIsLoading(true);
+
+        const purchaseOrder = await getPurchaseOrderById(id);
+
+        if (!purchaseOrder) {
+            console.error(`Purchase order with id ${id} not found`);
+            setIsLoading(false);
+            return;
+        }
+
+        setPurchaseOrderToReceive(purchaseOrder);
+        setIsLoading(false);
+
+        console.log('Receive PO:', id);
+    };
+
+    //navigate to PO receive screen
+    useEffect(() => {
+        if (purchaseOrderToReceive) {
+            setSelectedPurchaseOrder(purchaseOrderToReceive);
+            router.push('/receive-purchase-order');
+        }
+    }, [purchaseOrderToReceive]);
+
     return (
         <View style={{ flex: 1 }}>
             <PurchaseOrderList
                 poItems={purchaseOrders}
-                onReceivePo={() => {}}
+                onReceivePo={handleReceivePo}
                 onViewDetails={() => {}}
             />
         </View>
